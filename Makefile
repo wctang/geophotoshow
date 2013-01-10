c520f10647e78555641b5f7adef3af443b144cd2
wctang <wctang@gmail.com>
2013-01-10 19:14:22 +0800

    fix install process for appengine.

--- Makefile
+++ Makefile
@@ -6,7 +6,6 @@ else
 	sed -e "s/\/scripts\/flickr-maps.js/\/scripts\/flickr-maps.js?_=`date +%N`/" flickr.html > web/flickr.html
 	sed -e "s/\/scripts\/flickr-maps.js/\/scripts\/flickr-maps.js?_=`date +%N`/" show.html > web/show.html
 endif
-	cp *.py web
 	java -jar yuicompressor-2.4.2.jar flickr-maps-uncompress.js -o web/scripts/flickr-maps.js
 
 install: all
--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ read apikey
 echo Read API Secret
 read secret
 
-cp main.py /tmp/main.py
-sed -e "s/bde30c9a6b136fee3e6f7b9b6fa0117c/$apikey/" -e "s/7fbfea9ba5a11e5f/$secret/" /tmp/main.py > main.py
+cp web/main.py /tmp/main.py
+sed -e "s/bde30c9a6b136fee3e6f7b9b6fa0117c/$apikey/" -e "s/7fbfea9ba5a11e5f/$secret/" /tmp/main.py > web/main.py
 appcfg.py update .
-mv /tmp/main.py main.py
+mv /tmp/main.py web/main.py
 
 
